da8fc48d5c6460fa92842bfe3fa7d148f0b03958
Gerard <alu.gerard.pedrosa@mataro.epiaedu.cat>
2026-01-07 17:19:10 +0100

    Daus i posició

--- oca.java
+++ oca.java
@@ -21,20 +21,18 @@ public class oca {
         ordre(nomsJugadors);
 
         int [] casella = new int [nomsJugadors.length];
+        int[] penalitzacio = new int[nomsJugadors.length];
+        int jugadorActual = 0;
 
-        
         boolean finalJoc = false;
-        if (casella[0] == 63 || casella[1] == 63 || casella[2] == 63 || casella[3] == 63) {
-            
-            finalJoc = true;
-        }
-        
 
         while (!finalJoc) {
 
-            int [] torn = new int [nomsJugadors.length];
+        finalJoc = tornJugador(jugadorActual, nomsJugadors, casella, penalitzacio);
 
-            
+        jugadorActual = (jugadorActual + 1) % nomsJugadors.length;
+        System.out.println("**************************************************");
+        
         }
 
         System.out.println();
@@ -98,20 +96,52 @@ public class oca {
         }
     }
 
-    public int tirarDau() {
+    public int tirarUnDau() {
 
-        int tirada1 = (int)(Math.random() * 6) + 1;
-
-        int tirada2 = (int)(Math.random() * 6) + 1;
-
-        System.out.println("Has tret un " + tirada1 + " i un " + tirada2 + ".");
-        
-        return tirada1 + tirada2;
+    return (int)(Math.random() * 6) + 1;
     }
 
-    public void primerTorn() {
+    public int[] tirarDausSegonsCasella(int posicio) {
+    int dau1 = tirarUnDau();
+    int dau2 = 0;
 
+    if (posicio < 60) {
+        dau2 = tirarUnDau();
+    }
 
-        
+    return new int[]{dau1, dau2};
+    }
+
+    public boolean tornJugador(int jugadorActual, String[] nomsJugadors, int[] casella, int[] penalitzacio) {
+
+    if (penalitzacio[jugadorActual] > 0) {
+        penalitzacio[jugadorActual]--;
+        System.out.println(nomsJugadors[jugadorActual] + " perd un torn.");
+        return false;
+    }
+
+    System.out.println();
+    System.out.println("És el torn del jugador " + (jugadorActual + 1) + ", " + nomsJugadors[jugadorActual]);
+    System.out.print(">> tiro");
+    e.nextLine();
+
+    int[] daus = tirarDausSegonsCasella(casella[jugadorActual]);
+    int dau1 = daus[0];
+    int dau2 = daus[1];
+    int suma = dau1 + dau2;
+
+    if (dau2 > 0) {
+        System.out.println("Has obtingut un " + dau1 + " i un " + dau2 + " = " + suma);
+    } else {
+        System.out.println("Has obtingut un " + dau1);
+    }
+
+    casella[jugadorActual] += suma;
+
+    if (casella[jugadorActual] > 63) {
+        casella[jugadorActual] = 63 - (casella[jugadorActual] - 63);
+    }
+
+    return casella[jugadorActual] == 63;
     }
 }
